7893dd9f6a51ad0273d68a24e00da03f88e66c02
Chris Dalke <chrisdalke@gmail.com>
2020-12-17 23:28:19 -0500

    Refactor Orange to be a Strawberry

--- react-app/src/Orange.js
+++ react-app/src/Orange.js
@@ -1,4 +1,5 @@
 import orange1 from './orange1.png';
+import strawberry1 from './strawberry1.png';
 import orange2 from './orange2.png';
 import './Orange.scss'
 
@@ -7,7 +8,7 @@ function Orange({
     onClick
 }) {
     return (<div className="Orange" onClick={onClick}>
-        <img src={orange1} style={{display: canClick ? 'block' : 'none'}}/>
+        <img src={strawberry1} style={{display: canClick ? 'block' : 'none'}}/>
         <img src={orange2} style={{display: !canClick ? 'block' : 'none'}}/>
     </div>)
 }
